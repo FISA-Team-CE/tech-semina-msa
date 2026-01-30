3eab5e6cfc878e28dabb83480a2179d540ee9c5b
caminobelllo <alstpgml0812@gmail.com>
2026-01-30 16:36:40 +0900

    Fix: 디렉토리 경로 오류 수정

--- core-payment-service/core-user-service/src/main/java/com/fisa/core_user_service/dto/RegisterRequest.java
+++
@@ -1,9 +0,0 @@
-package com.fisa.core_user_service.dto;
-
-public record RegisterRequest(
-
-        String username,
-        String realName,
-        String residentNo
-) {
-}
--- core-payment-service/core-user-service/src/main/java/com/fisa/core_user_service/dto/UserResponse.java
+++
@@ -1,8 +0,0 @@
-package com.fisa.core_user_service.dto;
-
-public record UserResponse(
-        String userUuid,
-        String maskedName,
-        String maskedResidentNo
-) {
-}
