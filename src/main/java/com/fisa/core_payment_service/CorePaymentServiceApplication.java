52876e4c3c504aeb1d2d801c54a0cae1c83be538
caminobelllo <alstpgml0812@gmail.com>
2026-01-30 12:14:46 +0900

    Chore: 메인 애플리케이션

---
+++ src/main/java/com/fisa/core_payment_service/CorePaymentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.fisa.core_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class CorePaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorePaymentServiceApplication.class, args);
+	}
+
+}
