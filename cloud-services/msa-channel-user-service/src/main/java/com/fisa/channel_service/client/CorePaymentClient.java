45c228744e5cb760824a6ac583875f570730e534
caminobelllo <alstpgml0812@gmail.com>
2026-02-02 20:16:43 +0900

    Feat: 통신용 client 구현

---
+++ cloud-services/msa-channel-user-service/src/main/java/com/fisa/channel_service/client/CorePaymentClient.java
@@ -0,0 +1,29 @@
+package com.fisa.channel_service.client;
+
+import com.fisa.channel_service.dto.payment.AmountRequestDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.math.BigDecimal;
+
+// Core-Payment 호출 (On-Premise)
+@FeignClient(name = "core-payment-service", url = "http://localhost:8081")
+public interface CorePaymentClient {
+
+    // 계좌 개설
+    @PostMapping("/api/core/payment/accounts")
+    String createAccount(@RequestParam("accountNo") String accountNo,
+                         @RequestParam("userUuid") String userUuid);
+
+    // 입금 요청
+    @PostMapping("/api/core/payment/accounts/{accountNo}/deposit")
+    BigDecimal deposit(@PathVariable("accountNo") String accountNo,
+                       @RequestBody AmountRequestDto request);
+
+    // 출금 요청
+    @PostMapping("/api/core/payment/accounts/{accountNo}/withdraw")
+    BigDecimal withdraw(@PathVariable("accountNo") String accountNo,
+                        @RequestBody AmountRequestDto request);
+}
