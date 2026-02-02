07bb8444db96f9010daf2d5b1d63f107cd858c3a
caminobelllo <alstpgml0812@gmail.com>
2026-02-02 20:14:49 +0900

    Feat: 로그인 dto 정의

---
+++ cloud-services/msa-channel-user-service/src/main/java/com/fisa/channel_service/dto/user/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.fisa.channel_service.dto.user;
+
+public record LoginRequestDto(String loginId, String password) {
+}
---
+++ cloud-services/msa-channel-user-service/src/main/java/com/fisa/channel_service/dto/user/LoginResponseDto.java
@@ -0,0 +1,4 @@
+package com.fisa.channel_service.dto.user;
+
+public record LoginResponseDto(String accessToken) {
+}
