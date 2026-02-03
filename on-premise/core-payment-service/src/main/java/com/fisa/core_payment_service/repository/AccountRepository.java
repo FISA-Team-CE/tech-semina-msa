879daa73fb37f72bbea4255e2d64187e9a070771
caminobelllo <alstpgml0812@gmail.com>
2026-02-03 17:48:29 +0900

    Feat: uuid로 계좌 조회하는 메서드 추가

--- on-premise/core-payment-service/src/main/java/com/fisa/core_payment_service/repository/AccountRepository.java
+++ on-premise/core-payment-service/src/main/java/com/fisa/core_payment_service/repository/AccountRepository.java
@@ -4,4 +4,7 @@ import com.fisa.core_payment_service.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
+
+    // uuid로 계좌 조회
+    Optional<Account> findByUserUuid(String userUuid);
 }
