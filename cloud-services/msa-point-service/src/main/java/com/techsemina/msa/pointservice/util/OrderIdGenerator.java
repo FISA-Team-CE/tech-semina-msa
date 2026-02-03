517339ad52a65c118dcdaaa304a2d3f8f2df3456
juyeonBaeck <juyeon.it@gmail.com>
2026-02-03 18:00:24 +0900

    Refactor: 코드래빗 수정2

--- cloud-services/msa-point-service/src/main/java/com/techsemina/msa/pointservice/util/OrderIdGenerator.java
+++ cloud-services/msa-point-service/src/main/java/com/techsemina/msa/pointservice/util/OrderIdGenerator.java
@@ -2,19 +2,16 @@ package com.techsemina.msa.pointservice.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class OrderIdGenerator {
 
     // 결과 예시: PAY-20260203-163055-19283
     public static String generateOrderId() {
-        // 1. 날짜/시간 (초 단위까지)
-        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
 
-        // 2. 랜덤 숫자 (5자리) -> 실제로는 Redis 등에서 번호를 따오는 게 정석이지만, 지금은 랜덤으로
-        int randomNo = ThreadLocalRandom.current().nextInt(10000, 99999);
-
-        // 3. 조합
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmssSSS"));
+        String randomNo = UUID.randomUUID().toString().replace("-", "").substring(0, 12);
         return "PAY-" + timestamp + "-" + randomNo;
     }
 }
