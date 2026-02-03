4745174b377db879359e00b00c25556d69035c00
caminobelllo <alstpgml0812@gmail.com>
2026-02-03 17:49:05 +0900

    Refactor: dto validation 추가

--- on-premise/core-payment-service/src/main/java/com/fisa/core_payment_service/dto/AmountRequestDto.java
+++ on-premise/core-payment-service/src/main/java/com/fisa/core_payment_service/dto/AmountRequestDto.java
@@ -1,6 +1,18 @@
 package com.fisa.core_payment_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
-public record AmountRequestDto(String userUuid, BigDecimal amount) {
+public record AmountRequestDto(
+
+        @NotBlank(message = "사용자 UUID는 필수입니다.")
+        String userUuid,
+
+        @NotNull(message = "금액은 필수입니다.")
+        @Positive(message = "금액은 0보다 커야 합니다.")
+        BigDecimal amount
+) {
 }
